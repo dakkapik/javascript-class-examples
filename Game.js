e65830c62cdfe0593a197eff2a1a8db1aa9c6e6d
Felipe Rodas <68298003+dakkapik@users.noreply.github.com>
2022-11-07 11:38:57 -0500

    update

--- Game.js
+++ Game.js
@@ -1,23 +1,47 @@
 class Game {
-    constructor () {
-      this.gameWidth = 400;
-      this.gameHeight = 400;
-      this.floorY = 300;
-      this.gravity = 0.5;
-      this.keyPressed = new Set();
-      
-      document.addEventListener("keydown", (e) => {
-        this.keyPressed.add(e.keyCode)
-      })
-      
-      document.addEventListener("keyup", (e) => {
-        this.keyPressed.delete(e.keyCode)
-      })
-    }
+  constructor () {
+    this.gameWidth = 400;
+    this.gameHeight = 400;
+    this.floorY = 300;
+    this.gravity = 0.5;
+    this.keyPressed = new Set();
+    this.entities = {};
     
-    drawFloor () {
-      fill('white')
-      rect(0, this.floorY, this.gameWidth, this.gameHeight - this.floorY)
-    }
-}
+    document.addEventListener("keydown", (e) => {
+      this.keyPressed.add(e.keyCode)
+    })
+    
+    document.addEventListener("keyup", (e) => {
+      this.keyPressed.delete(e.keyCode)
+    })
+  }
   
+  addEntity (x, y, width, height) {
+    let id = Date.now().toString();
+    this.entities[id] = new Box(x, y, width, height, id);
+  }
+  
+  addPlayer () {
+    let id = Date.now().toString();
+    this.entities[id] = new Player(id);
+  }
+  
+  update() {
+    Object.keys(this.entities).forEach((id)=> {
+      this.entities[id].update()
+    })
+  }
+  
+  collision (id) {
+     
+  }
+  
+  drawFloor () {
+    fill('white')
+    rect(0, this.floorY, this.gameWidth, this.gameHeight - this.floorY)
+  }
+}
+
+
+
+
--- Player.js
+++ Player.js
@@ -1,74 +1,109 @@
 class Player {
-    constructor () {
-      this.x = 100;
-      this.y = 0;
-      this.xVelocity = 0;
-      this.yVelocity = 0;
-      this.grounded = false; 
-      this.width = 20;
-      this.height = 40;
-      this.runAccel = 5;
-      this.jumpHeight = 10;
-      this.attack = new Attack(20,10)
-      this.keys = {
-        left: 65,
-        right:68,
-        jump:32,
-        attack: 70
-      }
+  constructor (id) {
+    this.sprites = {
+      idle: loadImage('assets/char_idle.png'),
+      attack: loadImage('assets/char_attack.png')
     }
+    this.id = id;
+    this.x = 150;
+    this.y = 0;
+    this.xVelocity = 0;
+    this.yVelocity = 0;
+    this.grounded = false; 
+    this.width = 40;
+    this.height = 80;
+    this.topSpeed = 8;
+    this.jumpHeight = 10;
+    this.attacking = false;
     
-    update() {
-    
-      this.xVelocity = this.updateXVelocity()
-      this.yVelocity = this.updateYVelocity()
+    this.color = "rgba(0,0,255,0.2)";
   
-      this.x += this.xVelocity;
-      this.y += this.yVelocity;
-      this.draw();
-      if(game.keyPressed.has(this.keys.attack)) {
-        this.attack.draw(this.x + this.width, this.y);
-      }
+    this.attacks = {
+      punch : new Attack(30,30,10),  
+      kick: new Attack(30, 15, 10)
     }
     
-    updateXVelocity () {
-      if(game.keyPressed.has(this.keys.right)) return this.runAccel
-      if(game.keyPressed.has(this.keys.left)) return -this.runAccel
-      return 0;
-    }
-    
-    updateYVelocity () {
-      if(game.keyPressed.has(this.keys.jump) && this.grounded) {
-        this.grounded = false;
-        return -this.jumpHeight;
-      }
-      
-      if(this.y + this.height < game.floorY) {
-        this.grounded = false;
-        return this.yVelocity + game.gravity
-      }
-      
-      this.grounded = true;
-      return -(this.yVelocity / 2)
-    }
-    
-    draw(){
-      fill('blue')
-      rect(this.x, this.y, this.width, this.height)
+    this.keys = {
+      left: 65,
+      right:68,
+      jump:32,
+      punch: 70,
+      kick: 82
     }
   }
   
-  class Attack {
+  update() {
   
-    constructor(width, height) {
-      this.width = width;
-      this.height = height;
+    this.xVelocity = this.updateXVelocity()
+    this.yVelocity = this.updateYVelocity()
+
+    this.x = this.x + this.xVelocity;
+    this.y = this.y + this.yVelocity;
+    
+    this.drawHitbox();
+    this.draw();
+    
+    if(game.keyPressed.has(this.keys.punch)) {
+      this.attacking = true;
+      this.attacks.punch.draw(this.x + this.width, this.y);
+    } else {
+      this.attacking = false;
     }
     
-    draw(x, y) {
-      fill('red')
-      rect(x, y, this.width, this.height)
+    
+    if(game.keyPressed.has(this.keys.kick)) {
+      this.attacks.kick.draw(this.x + this.width, this.y);
+    }
+  }
+
+  
+  updateXVelocity () {
+    if(game.keyPressed.has(this.keys.right)) return this.topSpeed;
+    if(game.keyPressed.has(this.keys.left)) return -this.topSpeed;
+    return 0;
+  }
+  
+  updateYVelocity () {
+    if(game.keyPressed.has(this.keys.jump) && this.grounded) {
+      this.grounded = false;
+      return -this.jumpHeight;
     }
     
+    if(this.y + this.height < game.floorY) {
+      this.grounded = false;
+      return this.yVelocity + game.gravity;
+    }
+    
+    this.grounded = true;
+    return -(this.yVelocity / 2);
+  }
+  
+  drawHitbox(){
+    fill(this.color);
+    rect(this.x, this.y, this.width, this.height);
+  }
+
+  draw(){
+    if(this.attacking){
+      image(this.sprites.attack, this.x,this.y)
+    }else {
+      image(this.sprites.idle, this.x,this.y)
+    }
+    
+  }
 }
+
+class Attack {
+
+  constructor(width, height, yOffSet = 0) {
+    this.width = width;
+    this.height = height;
+    this.yOffSet = yOffSet;
+  }
   
+  draw(x, y) {
+    fill('rgba(255,0,0,0.2)');
+    rect(x, y + this.yOffSet, this.width, this.height);
+  }
+  
+}
